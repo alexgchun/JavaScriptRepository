35f605d94a428f231e2d51cfa7de58af3eb14e84
alexgchun <90280800+alexgchun@users.noreply.github.com>
2021-09-25 17:22:18 -0700

    new

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/Main.js
@@ -0,0 +1,28 @@
+document.write("It's ya boi J-Roc.");
+
+var a = "This is a string";
+
+document.write(a);
+
+var b = "Nother string";
+ 
+
+window.alert(b);
+//sets an alert with text nother string.
+document.write("Yo, i said, \"I wanted a canoli wid\' NO cream cheese ight?\"")
+
+document.write("\"I like my chair," + " but it ain\'t very comfty.\"" + "-The Hoff twins.");
+
+var c = "Concatenated" + " DONDADDA.";
+document.write(c);
+
+var family = "The Chuns", dad = "Brian", mom = "Esther", me = "Alex", orphan = "Paul";
+document.write(mom);
+
+var d = "i am dark color";
+var d = d.fontcolor("purple");
+document.write(d);
+
+var q = 5;
+var w = 10;
+document.write(q + w);
